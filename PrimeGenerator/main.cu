c9fd458825b58db844c22ffaea3b55bc65091e8f
slwilliams <slw69@cam.ac.uk>
2013-04-08 11:15:08 +0100

    Fixed code indentation.

--- PrimeGenerator/main.cu
+++ PrimeGenerator/main.cu
@@ -4,10 +4,10 @@ __global__ void checkPrime(int * d_in)
 {
   //get thread id
   int id = blockIdx.x + threadIdx.x;  
-  	
-	//num is now in local memory, much quicker
+    
+  //num is now in local memory, much quicker
   int num = d_in[id];
-	
+  
   //couple of corner cases
   if(num == 0 || num == 1)
   {
@@ -15,73 +15,71 @@ __global__ void checkPrime(int * d_in)
     return;
   }
   
-	//assume prime until proven otherwise
-	d_in[id] = 1;		
-	
-	//quicker to check 2 on its own
-	//then we can count up in 2s (only need to check odd numbers) starting from 3	
-	if(num % 2 == 0)
-	{
-		d_in[id] = 0;
-	}
-	else
-	{
+  //assume prime until proven otherwise
+  d_in[id] = 1;    
+  
+  //quicker to check 2 on its own
+  //then we can count up in 2s (only need to check odd numbers) starting from 3  
+  if(num % 2 == 0)
+  {
+    d_in[id] = 0;
+  }
+  else
+  {
     //only need to check upto ceil of sqrt(num)
     //better to start from 3 and count up rather than down    
     //do sqrt here not in loop to stop it being evaluated each time round
-		int sqrtNum = (int)sqrt((float)num);
+    int sqrtNum = (int)sqrt((float)num);
     
     for(int i = 3; i < sqrtNum + 1; i += 2)
-		{
-		  if(num % i == 0)
-		  {
+    {
+      if(num % i == 0)
+      {
         d_in[id] = 0;
         return;
-		  }		  
-		}
-	}
+      }      
+    }
+  }
 }
 
 int main(int argc, char ** argv)
 {
   //anything over 1000000 crashes it, not sure why
   //possibly to do with my vram
-	const int ARRAY_SIZE = 100000;
-	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
+  const int ARRAY_SIZE = 100000;
+  const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
 
-	//generate the input array on the host
-	int h_in[ARRAY_SIZE];
-	for (int i = 0; i < ARRAY_SIZE; i++) 
-	{
-		h_in[i] = i;
-	}	 
+  //generate the input array on the host
+  int h_in[ARRAY_SIZE];
+  for (int i = 0; i < ARRAY_SIZE; i++) 
+  {
+    h_in[i] = i;
+  }   
 
-	//declare GPU memory pointers
-	int * d_in;	
+  //declare GPU memory pointers
+  int * d_in;  
  
-	//allocate GPU memory
-	cudaMalloc((void**) &d_in, ARRAY_BYTES);	
+  //allocate GPU memory
+  cudaMalloc((void**) &d_in, ARRAY_BYTES);  
  
   //transfer the array to the GPU
-	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
-	//launch the kernel
+  //launch the kernel
   //not sure what the best ratio of blocks to threads is
-	checkPrime<<<ARRAY_SIZE/100, 100>>>(d_in);
+  checkPrime<<<ARRAY_SIZE/100, 100>>>(d_in);
 
-	//copy back the result array to the CPU
-	cudaMemcpy(h_in, d_in, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+  //copy back the result array to the CPU
+  cudaMemcpy(h_in, d_in, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
-	//print out the resulting array of primes
-	for (int i = 0; i < ARRAY_SIZE; i++)
-	{
+  //print out the resulting array of primes
+  for (int i = 0; i < ARRAY_SIZE; i++)
+  {
     if(h_in[i])
-    {
       printf("%d\n", i);
-    }		
-	}
+  }
 
-	cudaFree(d_in);
+  cudaFree(d_in);
   
-	return 0;
+  return 0;
 }
